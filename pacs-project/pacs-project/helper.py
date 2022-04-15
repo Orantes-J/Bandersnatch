0e25ad712ab6fc7b262cc9539bb1545007f87432
Juan Carlos Orantes <72782842+Orantes-J@users.noreply.github.com>
2022-04-15 00:15:18 -0700

    Deleted

--- pacs-project/pacs-project/helper.py
+++
@@ -1,7 +0,0 @@
-def astrid():
-    print('*'*116)
-def indent():
-    print('\n')
-
-good_vibes = ['good', 'alright', 'chill', 'decent']
-bad_vibes = ['complicated', 'dumb', 'stressful', 'overwhelming', 'bad']
--- pacs-project/pacs-project/path1.py
+++
@@ -1,5 +0,0 @@
-def a(att):
-    return f'\nI am glad you are having a {att} day. '
-
-def b():
-    user_input = input('\nDo you have any hobbies? ')
